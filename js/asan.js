bca5089709abdade0e6d05d06a3a09a2425b5439
oktobat <62067363+oktobat@users.noreply.github.com>
2020-10-23 00:38:08 +0900

    Add files via upload

--- js/asan.js
+++ js/asan.js
@@ -1,8 +1,7 @@
 (function ($) {
-  // 로고를 클릭하면 main.html의 #content를 load() 하시오.
   $("#wrap").on(
     "click",
-    "#header h1 a, #footer .quickMenu a, .mainContent #step_area a, .medicalContent .mediList a, .contTit .prev a",
+    "#header h1 a, #footer .quickMenu a, .mainContent #step_area a, .contTit .prev a",
     function () {
       var url = this.href;
       $("#container > #content").remove();
@@ -11,12 +10,48 @@
     }
   );
 
+  $("#wrap").on("click", ".medicalContent .mediList a", function () {
+    var url = this.href;
+    $("#container > #content").remove();
+    $("#container").load(url + " #content");
+
+    $.ajax({
+      type: "GET",
+      url: "data/doctors.json",
+
+      beforeSend: function (xhr) {
+        if (xhr.overrideMimeType) {
+          xhr.overrideMimeType("application/json");
+        }
+      },
+      success: function (data) {
+        var usedata = data.part1;
+        function dataPrint() {
+          var newContent = "";
+          for (var i in usedata) {
+            newContent += `<li><div class='img'><img src='${usedata[i].photo}' alt=''></div>`;
+            newContent += `<div class='doctorInfo'><strong>${usedata[i].name}</strong>`;
+            newContent += `<p>${usedata[i].depart}</p>`;
+            newContent += `<div>${usedata[i].about}</div></div></li>`;
+          }
+          $("#content .part1DoctorList").html(`<ul>${newContent}</ul>`);
+        }
+        dataPrint();
+      },
+      error: function () {
+        alert("큰일");
+      },
+    });
+
+    return false;
+  });
+
   $(window).scroll(function () {
     var sct = $(this).scrollTop();
     if (sct >= 50 && !$("#header").hasClass("on")) {
-      $("#header").addClass("on").slideUp(10).slideDown(10);
+      $("#header").stop().slideUp(100).stop().slideDown(100).addClass("on");
     } else if (sct < 50 && $("#header").hasClass("on")) {
-      $("#header").removeClass("on").slideUp(10).slideDown(10);
+      $("#header").removeClass("on");
     }
   });
 })(jQuery);
