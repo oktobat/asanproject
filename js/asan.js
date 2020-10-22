53a156ee30f1512e1d96cdf4c77526f085f39df9
oktobat <62067363+oktobat@users.noreply.github.com>
2020-10-22 21:41:27 +0900

    Add files via upload

--- js/asan.js
+++ js/asan.js
@@ -2,7 +2,7 @@
   // 로고를 클릭하면 main.html의 #content를 load() 하시오.
   $("#wrap").on(
     "click",
-    "#header h1 a, #footer .quickMenu a, .mainContent #step_area a, .medicalContent .mediList a",
+    "#header h1 a, #footer .quickMenu a, .mainContent #step_area a, .medicalContent .mediList a, .contTit .prev a",
     function () {
       var url = this.href;
       $("#container > #content").remove();
@@ -18,10 +18,5 @@
     } else if (sct < 50 && $("#header").hasClass("on")) {
       $("#header").removeClass("on").slideUp(10).slideDown(10);
     }
-  });
-
-  $("body").on("click", ".contTit prev a", function () {
-    history.back(-1);
-    return false;
   });
 })(jQuery);
